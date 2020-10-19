ca4c0434731228ba37fdd5aacf420475faa897af
Olusegun Olorunfemi <soolorunfemi@gmail.com>
2020-10-19 00:06:54 -0400

    main

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -4,3 +4,10 @@ import scrape_mars
 
 # Create an instance of Flask
 app = Flask(__name__)
+
+hemisphere_image_urls = [
+    {"title": "Valles Marineris Hemisphere", "img_url": "/static/image/Valles Marineris Hemisphere"},
+    {"title": "Cerberus Hemisphere", "img_url": "/static/image/Cerberus Hemisphere"},
+    {"title": "Schiaparelli Hemisphere", "img_url": "/static/image/Schiaparelli Hemisphere"},
+    {"title": "Syrtis Major Hemisphere", "img_url": "/static/image/Syrtis Major Hemisphere"}
+]
--- Missions_to_Mars/scrape_mars.py
+++ Missions_to_Mars/scrape_mars.py
@@ -1,2 +1,7 @@
 from splinter import Browser
 from bs4 import BeautifulSoup as bs
+import time
+
+def init_browser():
+    executable_path = {"executable_path": "/web-scraping-challenge/Missions_to_Mars/chromedriver"}
+    return Browser("chrome", **executable_path, headless=False)
