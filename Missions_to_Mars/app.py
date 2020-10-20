8e71c0c565e570fffd5c0ab7e1a21349a88ea1d1
Olusegun Olorunfemi <soolorunfemi@gmail.com>
2020-10-19 20:59:32 -0400

    main

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -5,3 +5,14 @@ import scrape_mars
 # Create an instance of Flask
 app = Flask(__name__)
 
+#Establish mongodb connection
+mongo = PyMongo(app, uri="mongodb://localhost:27017//mars_app")
+
+# Route to render index.html template using data from Mongo
+@app.route("/")
+def home():
+    mars_news = mongo.db.mars.find_one()
+    return render_template("index.html", listings=listings)
+
+if __name__ == "__main__":
+    app.run(debug=True)
